a3b45f37d741a50e6dcddd56dfcfa2621b24ffaa
UlianaAsadulina <u.asadulina@icloud.com>
2025-03-21 11:43:51 -0400

    import styles for components

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,4 +1,7 @@
+import useStyles from "./styles.js"
+ 
 export default function Form () {
+    const classes = useStyles();
     return ( 
         <h1>
             Form
--- src/components/Posts/Post/Post.jsx
+++ src/components/Posts/Post/Post.jsx
@@ -1,4 +1,7 @@
+import useStyles from "./styles.js"
+ 
 export default function Post () {
+    const classes = useStyles();
     return ( 
         <h1>
             Post
