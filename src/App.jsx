4c836d0eef5a83aee16a20128f99b50b9ce14f31
UlianaAsadulina <u.asadulina@icloud.com>
2025-03-20 21:34:55 -0400

    base components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Container, AppBar, Typography, Grow, Grid2, SpeedDialIcon} from '@mui/material';
+
+import Form from "./components/Form/Form.jsx"
+import Posts from './components/Posts/Posts.jsx';
+import memories from "./images/memories.jpg"
+
+import useStyles from "./styles.js"
 
 function App() {
-  
+  const classes = useStyles();
 
   return (
     <>
-      <h1>App</h1>
+      <Container maxWidth='lg'>
+        <AppBar className={classes.appBar} position='static' color='inherit'>
+          <Typography className={classes.heading} variant='h2' align='center'>Memories</Typography>
+          <img className={classes.image} src={memories} alt="memories" height='60' />
+        </AppBar>
+        <Grow in>
+          <Container>
+            <Grid2 container justifyContent="space-between" alignItems="stretch" spacing={3}>
+              <Grid2 item xs={12} sm={7}>
+                <Posts />
+
+              </Grid2>
+              <Grid2 item xs={12} sm={4}>
+                <Form />
+              </Grid2>
+            </Grid2>
+          </Container>
+
+        </Grow>
+      </Container>
     </>
   )
 }
---
+++ src/components/Posts/Posts.jsx
@@ -0,0 +1,9 @@
+import Post from "./Post/Post.jsx";
+
+export default function Posts() {
+    return (<>
+        <h1> POSTS </h1>
+        <Post />
+        <Post />
+    </>);
+};
